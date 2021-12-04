bd76787f014c727f3f3322039de382b8a6d86aa2
jagan353 <jaganmohanreddyk19@gmail.com>
2021-12-04 18:13:47 +0530

    firstcommit

---
+++ H2World/src/test/java/com/example/H2World/H2WorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.H2World;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2WorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
